f29e829c8626b83ca21eb78c0f6dd8504b30fd10
Gustavo Novaro <gnovaro@gmail.com>
2020-08-28 00:56:45 +0200

    Improve html generate

--- public/index.php
+++ public/index.php
@@ -10,7 +10,15 @@ function check_proccess_run(string $name) : bool
     return ($return == 0) ? true : false;
 }
 ?>
-
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
+    <title>Server status</title>
+</head>
+<body>
 <div>
     <strong>MySQL/MariaDB</strong> is <?php echo (check_proccess_run('mysql')) ? '<span style="color:green">running</span>' : '<span style="color:red">stoped</span>';?>
 </div>
@@ -18,3 +26,5 @@ function check_proccess_run(string $name) : bool
 <div>
     <strong>Redis</strong> is <?php echo (check_proccess_run('redis-server')) ? '<span style="color:green">running</span>' : '<span style="color:red">stoped</span>';?>
 </div>
+</body>
+</html>
